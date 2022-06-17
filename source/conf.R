4ed419837d474e6a158f45af8b93beb57576e575
Haneesh Jindal <94955130+schosio@users.noreply.github.com>
2022-06-17 12:09:40 +0530

    Update conf.R

--- source/conf.R
+++ source/conf.R
@@ -1,7 +1,4 @@
 
 if (!requireNamespace(c("BiocManager", "ggrepel", "dplyr", "ggplot", "data.table"), quietly = TRUE))
 	install.packages(c("BiocManager", "ggrepel", "dplyr", "ggplot", "data.table"), repos="https://cloud.r-project.org")
-if (!requireNamespace("DESeq2", quietly = TRUE))
-	pkgs <- rownames(installed.packages())
-	BiocManager::install(pkgs, type = "source", checkBuilt = TRUE)
-	BiocManager::install("DESeq2")
+pkgs <- rownames(installed.packages());BiocManager::install(pkgs, type = "source", checkBuilt = TRUE);BiocManager::install("DESeq2")
