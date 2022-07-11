2274b456e965efb716590e1ddc0127e70e4541e3
Haneesh Jindal <94955130+schosio@users.noreply.github.com>
2022-07-11 11:14:40 +0530

    update

--- source/master_script.sh
+++ source/master_script.sh
@@ -74,9 +74,11 @@ for infile in *1.fastq
         R1_unpaired=${name}_1unpaired.fastq
         R2_paired=${name}_2paired.fastq
         R2_unpaired=${name}_2unpaired.fastq
-        java -jar $HOME/C_files/application/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads $threads -phred33 -summary \
-        ${name}_statsSummaryFile.txt $infile ${name}_2.fastq $R1_paired $R1_unpaired $R2_paired $R2_unpaired \
-        ILLUMINACLIP:$HOME/C_files/application/Trimmomatic-0.39/adapters/TruSeq3-PE.fa:2:40:15 LEADING:28 TRAILING:28 AVGQUAL:28 MINLEN:50 
+        java -jar $HOME/C_files/application/Trimmomatic-0.39/trimmomatic-0.39.jar \
+        PE -threads $threads -phred33 -summary ${name}_statsSummaryFile.txt \
+        $infile ${name}_2.fastq $R1_paired $R1_unpaired $R2_paired $R2_unpaired \
+        ILLUMINACLIP:$HOME/C_files/application/Trimmomatic-0.39/adapters/TruSeq3-PE.fa:2:40:15 \
+        LEADING:28 TRAILING:28 AVGQUAL:28 MINLEN:50 
 done
 
 #sorting the output files in different directories
@@ -125,11 +127,11 @@ do
   	name=$(echo $i | awk -F"_" '{print $1}')
         R1_pair=${name}_1paired.fastq
         R2_pair=${name}_2paired.fastq
-        #display the command used
-        #options for hisat: -p is for threads used, --known-splicesite-infile is for splice site file
-        #options for samtools: view is for file conversion, -bS is for .bam as output and .sam as input
-        #options for samtools: sort is for sorting, -n is sorting by name, -o is for output
-        #options for samtools: fixmate is for fix mate information, markdup is for marking duplicates
+        # display the command used
+        # options for hisat: -p is for threads used, --known-splicesite-infile is for splice site file
+        # options for samtools: view is for file conversion, -bS is for .bam as output and .sam as input
+        # options for samtools: sort is for sorting, -n is sorting by name, -o is for output
+        # options for samtools: fixmate is for fix mate information, markdup is for marking duplicates
         hisat2 -p $threads -x $idx --known-splicesite-infile $splicefile -1 $R1_pair -2 $R2_pair \
                 | samtools view -@ $threads -bS - \
                 | samtools sort -@ $threads -n - -o $name.sorted.bam
@@ -153,8 +155,9 @@ rm *.fastq
 for i in *rmPCRdup.bam
 do
   	name=$(echo $i | awk -F".rmPCRdup" '{print $1}')
-        #options for stringtie: -G is for annotation file, -o is for output gtf file, -p is or threads, -b is for location of ballown table files
-        #options for stringtie: -A is for output of gene abundance file
+        # options for stringtie: -G is for annotation file, -o is for output 
+        ## gtf file, -p is or threads, -b is for location of ballown table files
+        # options for stringtie: -A is for output of gene abundance file
         stringtie $i -G ${annotation} -o $name.gencode.gtf -p $threads 
 done
 
@@ -163,7 +166,8 @@ done
 out_file=data_merge_annotation.gtf
 ls *gencode.gtf > temp.gtf.list
 
-stringtie --merge -p $threads -G $annotation -m 200 -F 1 -c 200 -o $out_file temp.gtf.list 
+stringtie --merge -p $threads -G $annotation -m 200 -F 1 -c 200 \
+-o $out_file temp.gtf.list 
 
 rm temp.gtf.list
 #STEP 8 comparing the information in merged gtf from stringtie and annotation file
@@ -191,7 +195,9 @@ gffread -w common_novel_transcript_seq.fa -g $idx common_novel_transcript.gtf
 
 # run CPAT
 
-cpat.py -x $scr_dir/files/Human_Hexamer_hg38.tsv --antisense -d $scr_dir/files/Human.logit.RData --top-orf=5 -g common_novel_transcript_seq.fa -o coding_potential_output
+cpat.py -x $scr_dir/files/Human_Hexamer_hg38.tsv --antisense \
+-d $scr_dir/files/Human.logit.RData --top-orf=5 -g common_novel_transcript_seq.fa \
+-o coding_potential_output
 
 # run htseq count
 mkdir htseq_files
@@ -199,7 +205,8 @@ mkdir htseq_files
 for i in *rmPCRdup.bam
 do
   	name=$(echo $i | awk -F".rmPCRdup" '{print $1}')
-        htseq-count -f bam $i -a 30 -s no -i transcript_id common-merged.annotated.gtf > ${name}.count.txt
+        htseq-count -f bam $i -a 30 -s no -i transcript_id common-merged.annotated.gtf \
+        > ${name}.count.txt
 done
 
 mv *.count.txt htseq_files/
